6cae91155dacc3c9c8dc5dd7e034522268b45bae
Piyush Singh <Piyush.Singh@utc.com>
2020-07-08 01:28:53 +0530

    modified code

--- functions/File9.js
+++ functions/File9.js
@@ -14,34 +14,36 @@
     - we will save TypeScript files by using ".ts" extension.
 */
 
-/*
-    function fun_one(arg1?:string,
-                    arg2?:string,
-                    arg3?:string):void{
-        console.log(arg1,arg2,arg3);
-    };
-    fun_one();                                  //undefined undefined undefined
-    fun_one("ReactJS");                         //ReactJS undefined undefined
-    fun_one("React Native","NodeJS","MongoDB"); //React Native NodeJS MongoDB
-    fun_one(undefined,
-            undefined,
-            undefined);                         //undefined undefined undefined
-    fun_one(null,
-            null,
-            null);                              //null null null
-*/
-/*
-//combination of normal with optional
-function fun_one(arg1:any,arg2?:any):void{
-    console.log(arg1,arg2);
+
+/* function fun_one(arg1?: string,
+    arg2?: string,
+    arg3?: string): void {
+    console.log(arg1, arg2, arg3);
 };
-//fun_one();                    //Expected 1-2 arguments, but got 0.
-fun_one("Hello_1");             //Hello_1 undefined
-fun_one("Hello_1","Hello_2");   //Hello_1 Hello_2
-fun_one(undefined,undefined);   //undefined undefined
-fun_one(null,null);             //null null
-*/
-//normal,default,optional and rest
+fun_one(); //undefined undefined undefined
+fun_one("ReactJS"); //ReactJS undefined undefined
+fun_one("React Native", "NodeJS", "MongoDB"); //React Native NodeJS MongoDB
+fun_one(undefined,
+    undefined,
+    undefined); //undefined undefined undefined
+fun_one(null,
+    null,
+    null); //null null null
+
+
+//combination of normal with optional
+function fun_one(arg1: any, arg2?: any): void {
+    console.log(arg1, arg2);
+};
+fun_one(); //Expected 1-2 arguments, but got 0.
+fun_one("Hello_1"); //Hello_1 undefined
+fun_one("Hello_1", "Hello_2"); //Hello_1 Hello_2
+fun_one(undefined, undefined); //undefined undefined
+fun_one(null, null); //null null
+
+normal,
+default, optional and rest
+ */
 function fun_one(normal, Default, optional) {
     if (Default === void 0) { Default = "Hello_2"; }
     var rest = [];
